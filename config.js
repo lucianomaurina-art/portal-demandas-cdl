af45226c796c21dec29d42d3b1883671d0c9f3c5
lucianomaurina-art <lucianomaurina@gmail.com>
2026-09-01 11:38:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CDL_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://uswsagujxwcfvauulhzq.supabase.co",
+  supabaseAnonKey: "sb_publishable_WjKdUuVCm_fdgE67M4sEUA_BbPwvB-c"
 };
